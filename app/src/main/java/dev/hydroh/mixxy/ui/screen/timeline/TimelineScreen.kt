b862581e9b60680af932c0de64cf411540b42e8f
HydroH <iodizon@163.com>
2023-10-02 15:51:29 +0800

    feat: Add top app bar.

--- app/src/main/java/dev/hydroh/mixxy/ui/screen/timeline/TimelineScreen.kt
+++ app/src/main/java/dev/hydroh/mixxy/ui/screen/timeline/TimelineScreen.kt
@@ -2,7 +2,6 @@ package dev.hydroh.mixxy.ui.screen.timeline
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -14,11 +13,15 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
 import androidx.compose.material3.TabRowDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -26,6 +29,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.paging.LoadState
@@ -42,6 +46,7 @@ import dev.hydroh.mixxy.util.pagerTabIndicatorOffset
 import kotlinx.collections.immutable.persistentMapOf
 import kotlinx.coroutines.launch
 
+// Use accompanist swiperefresh until it's added to material3
 @Suppress("DEPRECATION")
 @OptIn(
     ExperimentalFoundationApi::class,
@@ -60,29 +65,43 @@ fun TimelineScreen(
     val pagerState = rememberPagerState { viewModel.tabs.count() }
     val noteActionDialogState = remember { NoteActionDialogState() }
     val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(
+        rememberTopAppBarState()
+    )
 
-    Column(modifier = Modifier.fillMaxWidth()) {
-        TabRow(
-            selectedTabIndex = pagerState.currentPage,
-            indicator = { tabPositions ->
-                TabRowDefaults.PrimaryIndicator(
-                    Modifier.pagerTabIndicatorOffset(pagerState, tabPositions)
-                )
-            },
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            viewModel.tabs.forEachIndexed { index, tab ->
-                Tab(
-                    selected = pagerState.currentPage == index,
-                    onClick = {
-                        coroutineScope.launch { pagerState.animateScrollToPage(index) }
-                    },
-                    text = { Text(tab.title) },
-                )
-            }
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    TabRow(
+                        selectedTabIndex = pagerState.currentPage,
+                        indicator = { tabPositions ->
+                            TabRowDefaults.PrimaryIndicator(
+                                Modifier.pagerTabIndicatorOffset(pagerState, tabPositions)
+                            )
+                        },
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        viewModel.tabs.forEachIndexed { index, tab ->
+                            Tab(
+                                selected = pagerState.currentPage == index,
+                                onClick = {
+                                    coroutineScope.launch { pagerState.animateScrollToPage(index) }
+                                },
+                                text = { Text(tab.title) },
+                            )
+                        }
+                    }
+                },
+                scrollBehavior = scrollBehavior
+            )
         }
+    ) { innerPadding ->
         HorizontalPager(
             state = pagerState,
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxWidth()
         ) { page ->
             val timeline = when (viewModel.tabs[page].timeline) {
                 Timeline.HOME -> viewModel.homeTimeline
@@ -93,14 +112,18 @@ fun TimelineScreen(
             val pagingItems = timeline.pager.collectAsLazyPagingItems()
 
             SwipeRefresh(
-                state = rememberSwipeRefreshState(isRefreshing = pagingItems.loadState.refresh is LoadState.Loading),
+                state = rememberSwipeRefreshState(
+                    isRefreshing = pagingItems.loadState.refresh is LoadState.Loading
+                ),
                 onRefresh = {
                     pagingItems.refresh()
                     timeline.invalidate()
                 }
             ) {
                 LazyColumn(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .nestedScroll(scrollBehavior.nestedScrollConnection),
                     contentPadding = PaddingValues(12.dp),
                 ) {
                     items(
@@ -122,7 +145,10 @@ fun TimelineScreen(
                                 },
                                 onClickReplyButton = {
                                     noteActionDialogState.noteAction =
-                                        NoteAction.Create("@${it.user.username} ", replyID = it.id)
+                                        NoteAction.Create(
+                                            "@${it.user.username} ",
+                                            replyID = it.id
+                                        )
                                 },
                                 onClickRenoteButton = {
                                     noteActionDialogState.noteAction = NoteAction.Renote(it)
@@ -135,10 +161,24 @@ fun TimelineScreen(
                                 emojis = emojisState,
                                 modifier = when (index) {
                                     0 ->
-                                        Modifier.clip(RoundedCornerShape(12.dp, 12.dp, 0.dp, 0.dp))
+                                        Modifier.clip(
+                                            RoundedCornerShape(
+                                                12.dp,
+                                                12.dp,
+                                                0.dp,
+                                                0.dp
+                                            )
+                                        )
 
                                     pagingItems.itemCount ->
-                                        Modifier.clip(RoundedCornerShape(0.dp, 0.dp, 12.dp, 12.dp))
+                                        Modifier.clip(
+                                            RoundedCornerShape(
+                                                0.dp,
+                                                0.dp,
+                                                12.dp,
+                                                12.dp
+                                            )
+                                        )
 
                                     else -> Modifier
                                 }
