76921fa7893211575cf4f317764b0e795b6d7ccc
HydroH <ixlesis@gmail.com>
2024-10-29 17:17:05 +0800

    Bump library versions, implement MfmText

--- mfm-compose/src/main/java/dev/hydroh/mfm/ui/MfmText.kt
+++ mfm-compose/src/main/java/dev/hydroh/mfm/ui/MfmText.kt
@@ -1,12 +1,12 @@
 package dev.hydroh.mfm.ui
 
-import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.text.InlineTextContent
 import androidx.compose.foundation.text.appendInlineContent
 import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateMapOf
@@ -14,6 +14,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.LinkAnnotation
 import androidx.compose.ui.text.ParagraphStyle
 import androidx.compose.ui.text.Placeholder
 import androidx.compose.ui.text.PlaceholderVerticalAlign
@@ -21,9 +22,10 @@ import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.style.TextIndent
+import androidx.compose.ui.text.withLink
 import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
 import androidx.core.graphics.drawable.toBitmap
@@ -43,12 +45,9 @@ import com.github.samunohito.mfm.api.node.MfmUrl
 @Composable
 fun MfmText(
     markdown: String,
-    emojiMap: Map<String, String>,
+    emojiUrlMap: Map<String, String>,
     modifier: Modifier = Modifier,
     style: TextStyle = LocalTextStyle.current,
-    onClickMention: (String) -> Unit = {},
-    onClickHashTag: (String) -> Unit = {},
-    onClickUrl: (String) -> Unit = {},
 ) {
     val colorScheme = MaterialTheme.colorScheme
     val annotatedString = remember {
@@ -60,44 +59,44 @@ fun MfmText(
     val inlineContentMap = remember {
         mutableStateMapOf<String, InlineTextContent>()
     }
-    val emojiAspectRatioMap = remember {
-        mutableStateMapOf<String, Float>()
+    val emojiDimsMap = remember {
+        mutableStateMapOf<String, Pair<Int, Int>>()
     }
-    LaunchedEffect(emojiAspectRatioMap.toMap()) {
-        annotatedString.getStringAnnotations(MfmNodeType.EmojiCode.name, 0, annotatedString.length).forEach {
-            val emoji = it.item
-            emojiMap[emoji]?.let { url ->
-                inlineContentMap[emoji] = InlineTextContent(
-                    placeholder = Placeholder(
-                        width = 1.2.em * (emojiAspectRatioMap[emoji]?:1f),
-                        height = 1.2.em,
-                        placeholderVerticalAlign = PlaceholderVerticalAlign.TextBottom
-                    )
-                ) {
-                    AsyncImage(
-                        model = url,
-                        contentDescription = emoji,
-                        contentScale = ContentScale.FillHeight,
-                        onSuccess = {
-                            it.result.drawable.toBitmap().apply {
-                                emojiAspectRatioMap[emoji] = width.toFloat() / height.toFloat()
+    LaunchedEffect(emojiDimsMap.toMap()) {
+        annotatedString.getStringAnnotations(MfmNodeType.EmojiCode.name, 0, annotatedString.length)
+            .forEach {
+                val emoji = it.item
+                emojiUrlMap[emoji]?.let { url ->
+                    inlineContentMap[emoji] = InlineTextContent(
+                        placeholder = Placeholder(
+                            width = style.lineHeight * (emojiDimsMap[emoji]?.let {
+                                it.first.toFloat() / it.second.toFloat()
+                            } ?: 1f),
+                            height = style.lineHeight,
+                            placeholderVerticalAlign = PlaceholderVerticalAlign.TextBottom
+                        )
+                    ) {
+                        AsyncImage(
+                            model = url,
+                            contentDescription = emoji,
+                            contentScale = ContentScale.FillHeight,
+                            onSuccess = {
+                                it.result.drawable.toBitmap().apply {
+                                    emojiDimsMap[emoji] = Pair(width, height)
+                                }
                             }
-                        }
-                    )
+                        )
+                    }
                 }
             }
-        }
     }
     SelectionContainer {
-        ClickableText(text = annotatedString, modifier = modifier, style = style, onClick = {
-            annotatedString.getStringAnnotations(it, it).firstOrNull()?.let { annotation ->
-                when (annotation.tag) {
-                    MfmNodeType.Mention.name -> onClickMention(annotation.item)
-                    MfmNodeType.HashTag.name -> onClickHashTag(annotation.item)
-                    MfmNodeType.Url.name -> onClickUrl(annotation.item)
-                }
-            }
-        })
+        Text(
+            text = annotatedString,
+            modifier = modifier,
+            style = style,
+            inlineContent = inlineContentMap
+        )
     }
 }
 
@@ -106,45 +105,37 @@ fun IMfmNode.appendAnnotatedString(colorScheme: ColorScheme) {
     when (this) {
         is MfmText -> append(props.text)
         is MfmMention -> {
-            pushStringAnnotation(
-                tag = MfmNodeType.Mention.name,
-                annotation = props.acct
-            )
-            withStyle(SpanStyle(color = colorScheme.primary)) {
+            withLink(
+                LinkAnnotation.Url("mfm://accounts/${props.acct}")
+            ) {
                 append(props.acct)
             }
-            pop()
         }
+
         is MfmHashtag -> {
-            pushStringAnnotation(
-                tag = MfmNodeType.HashTag.name,
-                annotation = props.hashtag
-            )
-            withStyle(SpanStyle(color = colorScheme.primary)) {
+            withLink(
+                LinkAnnotation.Url("mfm://topics/${props.hashtag}")
+            ) {
                 append(stringify())
             }
-            pop()
         }
+
         is MfmUrl -> {
-            pushStringAnnotation(
-                tag = MfmNodeType.Url.name,
-                annotation = props.url
-            )
-            withStyle(SpanStyle(color = colorScheme.primary, textDecoration = TextDecoration.Underline)) {
-                append(props.url)
+            withLink(
+                LinkAnnotation.Url(props.url)
+            ) {
+                append(stringify())
             }
-            pop()
         }
+
         is MfmLink -> {
-            pushStringAnnotation(
-                tag = MfmNodeType.Url.name,
-                annotation = props.url
-            )
-            withStyle(SpanStyle(color = colorScheme.primary, textDecoration = TextDecoration.Underline)) {
+            withLink(
+                LinkAnnotation.Url(props.url)
+            ) {
                 children.forEach { it.appendAnnotatedString(colorScheme) }
             }
-            pop()
         }
+
         is MfmEmojiCode -> {
             pushStringAnnotation(
                 tag = MfmNodeType.EmojiCode.name,
@@ -153,13 +144,26 @@ fun IMfmNode.appendAnnotatedString(colorScheme: ColorScheme) {
             appendInlineContent(props.name, alternateText = stringify())
             pop()
         }
+
         is MfmBold -> withStyle(SpanStyle(fontWeight = FontWeight.Bold)) {
             children.forEach { it.appendAnnotatedString(colorScheme) }
         }
+
         is MfmQuote -> withStyle(ParagraphStyle(textIndent = TextIndent(8.sp, 8.sp))) {
             withStyle(SpanStyle(color = colorScheme.onSurface.copy(alpha = 0.6f))) {
                 children.forEach { it.appendAnnotatedString(colorScheme) }
             }
         }
     }
+}
+
+@Preview
+@Composable
+fun MfmTextPreview() {
+    MfmText(
+        markdown = "Hello, **world**! :smile:",
+        emojiUrlMap = mapOf(
+            "smile" to "https://example.com/smile.png"
+        )
+    )
 }
